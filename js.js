790ba0d1b5c7a2bd00a2aacb616ef5db2d600645
Daniilofficial <119125969+Daniilofficial@users.noreply.github.com>
2023-07-25 21:49:57 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector('.menu')
+let nav = document.querySelector('.nav')
+menu.addEventListener('click', function(){
+    nav.classList.toggle('active')
+    menu.classList.toggle('active')
+})
